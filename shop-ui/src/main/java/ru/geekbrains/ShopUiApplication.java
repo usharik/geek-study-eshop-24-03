c86eff77231589a0fea9c5fcb30738d1811f1ba1
usharik <usharik@mail.ru>
2021-04-14 18:41:31 +0200

    Lesson 4 preparation

--- shop-ui/src/main/java/ru/geekbrains/ShopUiApplication.java
+++ shop-ui/src/main/java/ru/geekbrains/ShopUiApplication.java
@@ -10,12 +10,6 @@ import org.springframework.data.redis.serializer.RedisSerializer;
 @SpringBootApplication
 public class ShopUiApplication {
 
-    @Bean
-    @Profile("!dev")
-    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
-        return new GenericJackson2JsonRedisSerializer();
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(ShopUiApplication.class, args);
     }
